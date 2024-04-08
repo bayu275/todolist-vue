39a21dff243998275f991689b9ee172574b37b77
Bayu <bayuusetiawan86@gmail.com>
2024-04-09 02:12:42 +0700

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <script setup></script>
 
 <template>
-  <h1>Hello World</h1>
+  <header>
+    <div class="container flex items-center justify-between py-4">
+      <a href="#" class="logo text-xl font-semibold"
+        >Bayu <span class="text-primary_alt">x Tasks</span></a
+      >
+      <div class="rounded-full bg-gradient-to-br from-white to-[#350084] p-px">
+        <button
+          class="flex items-center justify-center rounded-full bg-gradient-to-br from-[#ab83e7] to-[#350084] px-[12px] py-[4px] text-base shadow-button"
+        >
+          Powered by
+          <i class="bx bxl-vuejs translate-y-[2px]"></i>
+        </button>
+      </div>
+    </div>
+  </header>
 </template>
