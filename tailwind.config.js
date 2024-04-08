85911a1eb77063ce196ee05078e3ec2bf061f2ab
Bayu <bayuusetiawan86@gmail.com>
2024-04-09 02:03:11 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1a1621",
+        primary_alt: "#8259c0",
+        secondary: "#302640",
+        secondary_alt: "#350084",
+      },
+      boxShadow: {
+        button: "0 0 17px 0 #7030f7",
+        button_hover: "inset 0 0 0 100px rgba(0, 0, 0, 0.15)",
+      },
+    },
+    container: {
+      padding: "1rem",
+      center: true,
+    },
+  },
+  plugins: [],
+};
